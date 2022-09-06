57cb06e88fc3ca7f66acfd48448c8eeb06ce00dc
guest01 <guest01@ITELABD17>
2022-09-05 22:50:49 -0300

    Adicionar arquivos de projeto.

---
+++ appJs/app/JavaScript.js
@@ -0,0 +1,25 @@
+﻿function onClickSalvar() {
+
+    let campoNome = document.querySelector('#nome');
+    let campoIdade = document.querySelector('#idade');
+
+    if (!campoNome.value) {
+        alert('Nome não preenchido;');
+        return;
+    }
+
+    if (!campoIdade.value) {
+        alert('Idade não preeenchida.');
+        return;
+    }
+
+    var pessoa = {
+        nome: campoNome.value,
+        idade: campoIdade.value
+    };
+
+    let div = document.querySelector('#dadosSalvos');
+
+    div.innerHTML = ' <input disabled value = "' + pessoa.nome + '" />' +
+        ' <input disabled value = "' + pessoa.idade + '" /> '
+}
